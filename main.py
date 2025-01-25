05d96b31eb3c200a4b13191bf47539808d541488
voidCaloneial <void4function@gmail.com>
2025-01-25 09:50:00 +0500

    folder structure update + moving the main function to the main.py

---
+++ main.py
@@ -0,0 +1,23 @@
+from src.main import process_file
+
+
+data_file_path = 'src/data.json'
+
+def main():
+    """
+    Основная функция программы.
+    """
+    category_counts, category_sales = process_file(data_file_path)
+
+    # Выводим результаты
+    print("\nКоличество предметов по категориям:")
+    for category, count in category_counts.items():
+        print(f"{category}: {count}")
+
+    print("\nОбщая сумма продаж по категориям:")
+    for category, total in category_sales.items():
+        print(f"{category}: {total:.2f}")
+
+
+if __name__ == '__main__':
+    main()
